c4d72a3544c05e46806b9d1917405c4cda0d90be
Shawn Conboy <conboysbusiness@gmail.com>
2026-02-10 10:45:32 -0500

    Project finished. Possibly add a readMe. Added class library, linked reference, and finished database page

--- sConboyLab3/DatabaseForm.Designer.cs
+++ sConboyLab3/DatabaseForm.Designer.cs
@@ -114,6 +114,7 @@
             this.stateInformationBindingNavigator.BindingSource = this.stateInformationBindingSource;
             this.stateInformationBindingNavigator.CountItem = this.bindingNavigatorCountItem;
             this.stateInformationBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.stateInformationBindingNavigator.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.stateInformationBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.bindingNavigatorMoveFirstItem,
             this.bindingNavigatorMovePreviousItem,
@@ -134,7 +135,7 @@
             this.stateInformationBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
             this.stateInformationBindingNavigator.Name = "stateInformationBindingNavigator";
             this.stateInformationBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
-            this.stateInformationBindingNavigator.Size = new System.Drawing.Size(1250, 25);
+            this.stateInformationBindingNavigator.Size = new System.Drawing.Size(1250, 27);
             this.stateInformationBindingNavigator.TabIndex = 1;
             this.stateInformationBindingNavigator.Text = "bindingNavigator1";
             // 
@@ -144,13 +145,13 @@
             this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
             this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
             this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(24, 24);
             this.bindingNavigatorAddNewItem.Text = "Add new";
             // 
             // bindingNavigatorCountItem
             // 
             this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
-            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(39, 22);
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 24);
             this.bindingNavigatorCountItem.Text = "of {0}";
             this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
             // 
@@ -160,7 +161,7 @@
             this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
             this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
             this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(24, 24);
             this.bindingNavigatorDeleteItem.Text = "Delete";
             // 
             // bindingNavigatorMoveFirstItem
@@ -169,7 +170,7 @@
             this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
             this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
             this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(24, 24);
             this.bindingNavigatorMoveFirstItem.Text = "Move first";
             // 
             // bindingNavigatorMovePreviousItem
@@ -178,13 +179,13 @@
             this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
             this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
             this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(24, 24);
             this.bindingNavigatorMovePreviousItem.Text = "Move previous";
             // 
             // bindingNavigatorSeparator
             // 
             this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 27);
             // 
             // bindingNavigatorPositionItem
             // 
@@ -199,7 +200,7 @@
             // bindingNavigatorSeparator1
             // 
             this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
-            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 27);
             // 
             // bindingNavigatorMoveNextItem
             // 
@@ -207,7 +208,7 @@
             this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
             this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
             this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(24, 24);
             this.bindingNavigatorMoveNextItem.Text = "Move next";
             // 
             // bindingNavigatorMoveLastItem
@@ -216,20 +217,20 @@
             this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
             this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
             this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(24, 24);
             this.bindingNavigatorMoveLastItem.Text = "Move last";
             // 
             // bindingNavigatorSeparator2
             // 
             this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
-            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 27);
             // 
             // stateInformationBindingNavigatorSaveItem
             // 
             this.stateInformationBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.stateInformationBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("stateInformationBindingNavigatorSaveItem.Image")));
             this.stateInformationBindingNavigatorSaveItem.Name = "stateInformationBindingNavigatorSaveItem";
-            this.stateInformationBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 22);
+            this.stateInformationBindingNavigatorSaveItem.Size = new System.Drawing.Size(24, 24);
             this.stateInformationBindingNavigatorSaveItem.Text = "Save Data";
             this.stateInformationBindingNavigatorSaveItem.Click += new System.EventHandler(this.stateInformationBindingNavigatorSaveItem_Click);
             // 
@@ -253,6 +254,7 @@
             this.stateInformationDataGridView.DataSource = this.stateInformationBindingSource;
             this.stateInformationDataGridView.Location = new System.Drawing.Point(24, 241);
             this.stateInformationDataGridView.Name = "stateInformationDataGridView";
+            this.stateInformationDataGridView.RowHeadersWidth = 51;
             this.stateInformationDataGridView.Size = new System.Drawing.Size(1203, 451);
             this.stateInformationDataGridView.TabIndex = 2;
             // 
@@ -260,6 +262,7 @@
             // 
             this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
             this.dataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
             this.dataGridViewTextBoxColumn1.ReadOnly = true;
             // 
@@ -267,60 +270,70 @@
             // 
             this.dataGridViewTextBoxColumn2.DataPropertyName = "Name";
             this.dataGridViewTextBoxColumn2.HeaderText = "Name";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
             // 
             // dataGridViewTextBoxColumn3
             // 
             this.dataGridViewTextBoxColumn3.DataPropertyName = "Population";
             this.dataGridViewTextBoxColumn3.HeaderText = "Population";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
             // 
             // dataGridViewTextBoxColumn4
             // 
             this.dataGridViewTextBoxColumn4.DataPropertyName = "Flag_Description";
             this.dataGridViewTextBoxColumn4.HeaderText = "Flag_Description";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
             // 
             // dataGridViewTextBoxColumn5
             // 
             this.dataGridViewTextBoxColumn5.DataPropertyName = "Flower";
             this.dataGridViewTextBoxColumn5.HeaderText = "Flower";
+            this.dataGridViewTextBoxColumn5.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
             // 
             // dataGridViewTextBoxColumn6
             // 
             this.dataGridViewTextBoxColumn6.DataPropertyName = "Bird";
             this.dataGridViewTextBoxColumn6.HeaderText = "Bird";
+            this.dataGridViewTextBoxColumn6.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
             // 
             // dataGridViewTextBoxColumn7
             // 
             this.dataGridViewTextBoxColumn7.DataPropertyName = "Colors";
             this.dataGridViewTextBoxColumn7.HeaderText = "Colors";
+            this.dataGridViewTextBoxColumn7.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
             // 
             // dataGridViewTextBoxColumn8
             // 
             this.dataGridViewTextBoxColumn8.DataPropertyName = "Three_Largest_Cities";
             this.dataGridViewTextBoxColumn8.HeaderText = "Three_Largest_Cities";
+            this.dataGridViewTextBoxColumn8.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
             // 
             // dataGridViewTextBoxColumn9
             // 
             this.dataGridViewTextBoxColumn9.DataPropertyName = "Capitol";
             this.dataGridViewTextBoxColumn9.HeaderText = "Capitol";
+            this.dataGridViewTextBoxColumn9.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
             // 
             // dataGridViewTextBoxColumn10
             // 
             this.dataGridViewTextBoxColumn10.DataPropertyName = "Median_Income";
             this.dataGridViewTextBoxColumn10.HeaderText = "Median_Income";
+            this.dataGridViewTextBoxColumn10.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
             // 
             // dataGridViewTextBoxColumn11
             // 
             this.dataGridViewTextBoxColumn11.DataPropertyName = "Percent_Tech_Jobs";
             this.dataGridViewTextBoxColumn11.HeaderText = "Percent_Tech_Jobs";
+            this.dataGridViewTextBoxColumn11.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn11.Name = "dataGridViewTextBoxColumn11";
             // 
             // label1
@@ -397,7 +410,7 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.sortByDropdown);
-            this.groupBox1.Location = new System.Drawing.Point(261, 98);
+            this.groupBox1.Location = new System.Drawing.Point(835, 98);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(200, 100);
             this.groupBox1.TabIndex = 11;
@@ -418,7 +431,7 @@
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.filterDropdown);
-            this.groupBox3.Location = new System.Drawing.Point(789, 98);
+            this.groupBox3.Location = new System.Drawing.Point(215, 98);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(200, 100);
             this.groupBox3.TabIndex = 13;
@@ -441,7 +454,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "DatabaseForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "DatabaseForm";
+            this.Text = "State Database";
             this.Load += new System.EventHandler(this.DatabaseForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.stateInformationDBDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.stateInformationBindingSource)).EndInit();
--- sConboyLab3/DetailForm.cs
+++ sConboyLab3/DetailForm.cs
@@ -30,30 +30,25 @@ namespace sConboyLab3
             // go through each row to find the "selectedState"
             foreach (DataRow row in this.stateInformationDBDataSet.StateInformation.Rows)
             {
+                // find selected state's record, and assign data to textboxes
                 if (row["Name"].ToString() == selectedState)
                 {
                     nameTextBox.Text = row["Name"].ToString();
 
-                    populationTextBox.Text =
-                        Convert.ToInt32(row["Population"]).ToString("N0");
+                    populationTextBox.Text = Convert.ToInt32(row["Population"]).ToString("N0");
 
-                    flag_DescriptionTextBox.Text =
-                        row["Flag_Description"].ToString();
+                    flag_DescriptionTextBox.Text = row["Flag_Description"].ToString();
 
                     flowerTextBox.Text = row["Flower"].ToString();
                     birdTextBox.Text = row["Bird"].ToString();
                     colorsTextBox.Text = row["Colors"].ToString();
-                    three_Largest_CitiesTextBox.Text =
-                        row["Three_Largest_Cities"].ToString();
+                    three_Largest_CitiesTextBox.Text = row["Three_Largest_Cities"].ToString();
 
                     capitolTextBox.Text = row["Capitol"].ToString();
 
-                    median_IncomeTextBox.Text =
-                        Convert.ToDecimal(row["Median_Income"]).ToString("C0");
+                    median_IncomeTextBox.Text = Convert.ToDecimal(row["Median_Income"]).ToString("C0");
 
-                    percent_Tech_JobsTextBox.Text =
-                        Convert.ToDecimal(row["Percent_Tech_Jobs"]).ToString("0.0") + "%";
-
+                    percent_Tech_JobsTextBox.Text = Convert.ToDecimal(row["Percent_Tech_Jobs"]).ToString("0.0") + "%";
 
                     break;
                 }
